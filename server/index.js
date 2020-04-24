a031616234a053b444cea225eae74d64b655caaa
Francisco Salazar <franciscosalazarco@outlook.com>
2020-04-24 13:00:57 -0500

    finished

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.get("/", (req, res)=>{
     res.json({"hello": "I am happy to deploy our application"})
 })
 
-app.get("/api/user/auth", auth, (req,res) => {
+app.get("/api/users/auth", auth, (req,res) => {
     res.status(200).json({
         _id: req._id,
         isAuth: true,
@@ -45,7 +45,7 @@ app.post('/api/users/register', (req, res) => {
     
 });
 
-app.post('/api/user/login', (req, res) => {
+app.post('/api/users/login', (req, res) => {
     //find the email
     User.findOne({ email: req.body.email }, (err, user) => {
         if(!user)
@@ -72,7 +72,7 @@ app.post('/api/user/login', (req, res) => {
     });    
 });
 
-app.get("/api/user/logout", auth, (req, res) => {
+app.get("/api/users/logout", auth, (req, res) => {
     User.findOneAndUpdate({_id: req.user._id}, {token: ""}, (err, doc)=>{
         if(err) return res.json({ success: false, err})
         return res.status(200).send({
